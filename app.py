1b7fed7b03adef3dc5b72e5c8d469b7048688c8c
Ayen Agrawal <ayen.agr19@gmail.com>
2022-06-20 01:20:16 +0530

    Resolving heroku specific issues.

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from security import authenicate, identity
 from resources.user import UserRegistration
 from resources.store import Store, StoreList
 from resources.item import Item, Items
-from db import db
+# from db import db
 
 app = Flask(__name__)
 api = Api(app)
@@ -14,10 +14,6 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 jwt = JWT(app, authenicate, identity)
 
-@app.before_first_request
-def create_table():
-    db.create_all()
-
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores/')
 api.add_resource(Item, '/item/<string:name>')
@@ -25,5 +21,5 @@ api.add_resource(Items, '/items/')
 api.add_resource(UserRegistration, '/register')
 
 if __name__ == '__main__':
-    db.init_app(app)
+    # db.init_app(app)
     app.run(debug=True, port=5000)
