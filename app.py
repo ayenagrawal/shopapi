7c668f25977f40ec16761ae579cbfc455cc2e205
Ayen Agrawal <ayen.agr19@gmail.com>
2022-07-21 23:38:37 +0530

    Replaced bug in replacing postgres to postgresql

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 api = Api(app)
 app.secret_key = os.getenv('secret_key').encode('UTF-8')
 db_URI = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
-if db_URI.split()[0].startswith('postgres://'): db_URI.replace('postgres://', 'postgresql://', 1)
+if db_URI.split()[0].startswith('postgres://'): db_URI = db_URI.replace('postgres://', 'postgresql://', 1)
 app.config['SQLALCHEMY_DATABASE_URI'] = db_URI
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 jwt = JWT(app, authenicate, identity)
